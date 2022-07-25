777752c180e6fcbeb328f810eb94fcedd183ef3c
alvaroormeno <alvaro@ormeno.org>
2022-07-25 10:40:38 -0400

    added contact us text, hero left column desktop view basic design complete

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -9,12 +9,12 @@ const Home = () => {
       <main className='w-[100wv] h-screen pt-[150px]  mt-[-150px] bg-desktop-circle bg-no-repeat bg-[right_-200px_top_-300px] '>
 
 
-        {/* HERO SECTION */}
+        {/* [[[[[[ HERO SECTION ]]]]]] */}
         <div className=' flex flex-row mx-[150px] justify-center'>
           {/* Left Column */}
           <div className='flex flex-col justify-center align-middle w-[50%] gap-10'>
             {/* Main Text */}
-            <div className='text-[72px]  leading-[72px] font-[500] font-SerifDisplay'>
+            <div className='text-[72px]  leading-[72px] font-[500] font-SerifDisplay text-fmSanJuanBlue'>
               Start building with our APIs for absolutely free.
             </div>
             {/* Email Input & Button Row Container */}
@@ -25,6 +25,10 @@ const Home = () => {
               <button className=' bg-fmDarkPink text-[15px] font-[700] text-fmWaterWhite w-[173px] h-[48px] rounded-[50px]'>
                 Schedule a Demo
               </button>
+            </div>
+            {/* Question text */}
+            <div className='text-[15px] text-fmLightSanJuanBlue ml-6 mt-[-20px]'>
+              <p>Have any questions? <span className='font-bold text-fmSanJuanBlue'>Contact Us</span></p>
             </div>
 
 
