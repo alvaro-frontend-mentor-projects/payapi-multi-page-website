168d6df63a05895af6b1fafb887c651376215df2
alvaroormeno <alvaro@ormeno.org>
2022-07-31 22:36:11 -0400

    hero mobile render complete

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,82 +1,61 @@
-// IMPORT REACT, ROUTER-DOM, 
-import React, {useState} from 'react'
-import { Link } from 'react-router-dom'
+// IMPORT REACT, ROUTER-DOM,
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 // IMPORT IMAGES AND LOGS
-import logo from '../assets/shared/desktop/logo.svg'
-import hambuger from '../assets/shared/mobile/menu.svg'
-import closeMobile from '../assets/shared/mobile/close.svg'
-import bgCircle from '../assets/shared/desktop/bg-pattern-circle.svg'
-
-
-
+import logo from "../assets/shared/desktop/logo.svg";
+import hambuger from "../assets/shared/mobile/menu.svg";
+import closeMobile from "../assets/shared/mobile/close.svg";
+import bgCircle from "../assets/shared/desktop/bg-pattern-circle.svg";
 
 const Navbar = () => {
+	//MOBILE MENU STATE
+	const [mobile, setMobile] = useState(true);
 
-  //MOBILE MENU STATE
-  const [mobile, setMobile] = useState(false)
-
-
-
-  const handleMenu = function() {
-    setMobile(!mobile)
-    console.log("it works!")
-  }
-
-  const addEmployee2 = (e) => {
-    e.preventDefault();
-    console.log("it works!")
-  };
-
-  
+	const handleMenu = function () {
+		setMobile(!mobile);
+		console.log("it works!");
+	};
 
 
-  return (
+	return (
+		//// NAVBAR CONTAINER ////
+		<div className="w-[100vw] sm:h-[120px] md:h-[150px] flex flex-col justify-center items-center bg-transparent ">
+			{/* Flex Container */}
+			<div className="flex flex-row justify-between w-[80%] gap-10 items-center ">
+				{/* Logo */}
+				<img className="s" src={logo} alt="" />
+				{/* Links */}
+				<ul className="sm:hidden md:flex lg:flex flex-row gap-10 text-[15px] font-PublicSans font-[500] text-fmLightSanJuanBlue">
+					<li className="hover:text-fmSanJuanBlue z-10">
+						<Link to="/pricing">Pricing</Link>
+					</li>
+					<li className="hover:text-fmSanJuanBlue z-10">
+						<Link to="/about">About</Link>
+					</li>
+					<li className="hover:text-fmSanJuanBlue z-10">
+						<Link to="/contact">Contact</Link>
+					</li>
+				</ul>
+				{/* Button */}
+				<button className="sm:hidden md:block lg:block ml-auto font-PublicSans font-[500] bg-fmDarkPink hover:bg-fmCharmPink text-[15px]  text-fmWaterWhite w-[173px] h-[48px] rounded-[50px] z-10">
+					Schedule a Demo
+				</button>
 
-    // NAVBAR CONTAINER
-    <div className='w-[100vw] h-[150px] flex flex-col justify-center items-center bg-transparent '>
-      {/* Flex Container */}
-      <div className='flex flex-row justify-between w-[80%] gap-10 items-center '>
-        {/* Logo */}
-        <img className='s' src={logo} alt="" />
-        {/* Links */}
-        <ul className='sm:hidden md:flex lg:flex flex-row gap-10 text-[15px] font-PublicSans font-[500] text-fmLightSanJuanBlue'>
-          <li className='hover:text-fmSanJuanBlue z-10'><Link to="/pricing">Pricing</Link></li>
-          <li className='hover:text-fmSanJuanBlue z-10'><Link to="/about">About</Link></li>
-          <li className='hover:text-fmSanJuanBlue z-10'><Link to="/contact">Contact</Link></li>
-        </ul>
-        {/* Button */}
-        <button className='sm:hidden md:block lg:block ml-auto font-PublicSans font-[500] bg-fmDarkPink hover:bg-fmCharmPink text-[15px]  text-fmWaterWhite w-[173px] h-[48px] rounded-[50px] z-10'>
-          Schedule a Demo
-        </button>
-
-
-        
-
-        {/* MOBILE MENU SECTION */}
-        <div className='hidden' onClick={handleMenu} >
-          {mobile ?  
-          <img 
-          className='' 
-          src={hambuger} 
-          alt="hamburger menu icon"
-        />
-          : 
-         <img
-          className=''
-          src={closeMobile} 
-          alt="close menu icon" 
-        />  
-        }
+				{/* MOBILE MENU SECTION */}
+				<div className="hidden" onClick={handleMenu}>
+					{mobile ? (
+						<img className="" src={hambuger} alt="hamburger menu icon" />
+					) : (
+						<img className="" src={closeMobile} alt="close menu icon" />
+					)}
+				</div>
+        {/* Pop up menu container */}
+        <div>
           
         </div>
+			</div>
+		</div>
+	);
+};
 
-        {/* <img className='absolute h-[700px] w-[700px] z-[-1]'  src={bgCircle} alt="" /> */}
-        
-        
-
-      </div>
-    </div>
-  )
-}
-
-export default Navbar
+export default Navbar;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -21,30 +21,30 @@ const Home = () => {
 
       {/* Circle Bacckground image - absolute position and using index-z of 0 to be places behind all other elements with z-index of 10  - img relative parent is main container with overflow-x-hidden */}
       <div className='flex'>
-        <img className='absolute md:max-w-[1500px] md:w-[1000px] md:right-[-120px] md:top-[-720px] lg:w-[780px] lg:right-[-150px] lg:top-[-150px] z-0' src={bgCircle} alt="" />
+        <img className='absolute sm:max-w-[1500px] sm:w-[780px] md:w-[1000px] sm:left-[-202px] sm:top-[-550px] md:right-[-120px] md:top-[-720px] lg:w-[780px] lg:left-[850px] lg:top-[-180px] z-0' src={bgCircle} alt="" />
       </div>
       
 
 
         {/* [[[[[[ HERO SECTION ]]]]]] */}
-        <div className=' flex md:flex-col-reverse lg:flex-row md:mx-[80px] lg:mx-[150px] md:justify-end lg:justify-center items-center md:h-[670px] lg:h-[600px] '>
+        <div className=' flex sm:flex-col-reverse md:flex-col-reverse lg:flex-row md:mx-[80px] lg:mx-[150px] md:justify-end lg:justify-center items-center  md:h-[670px] lg:h-[600px] '>
           {/* Left Column */}
           <div className='flex flex-col justify-center align-middle md:w-[100%] lg:w-[50%] gap-10'>
             {/* Main Text */}
-            <div className='md:text-[48px] md:text-center lg:text-[72px] md:leading-[56px] lg:leading-[72px] font-[500] font-SerifDisplay text-fmSanJuanBlue md:mt-[-40px] lg:mt-[0] md:px-[20px] lg:px-[0px]'>
-              Start building with our APIs for absolutely free.
+            <div className='  sm:w-[325px] md:w-[100%] sm:text-[32px] md:text-[48px] sm:text-center md:text-start lg:text-[72px] sm:leading-[36px] md:leading-[56px] lg:leading-[72px] md:font-[500] font-SerifDisplay sm:mt-[-25px] md:mt-[-40px] lg:mt-[0] md:px-[20px] lg:px-[0px]  text-fmSanJuanBlue'>
+              <p> Start building with our APIs for absolutely free.</p>
             </div>
             {/* Email Input & Button Row Container */}
-            <div className='flex flex-row md:justify-center lg:justify-start '>
+            <div className='flex sm:flex-col sm:items-center md:item md:flex-row md:justify-center lg:justify-start sm:gap-[15px] md:gap-0 sm:mt-[-20px]'>
               {/* Email */}
-              <input className='rounded-[50px] pl-5 pr-20 mr-[-35px] text-fmSanJuanBlue font-PublicSans font-[500] focus:outline-none z-10' type="text" placeholder='Enter email address' />
+              <input className='rounded-[50px] pl-5 pr-20 sm:mr-0 md:mr-[-35px] w-[327px] h-[48px] text-fmSanJuanBlue font-PublicSans font-[500] focus:outline-none z-10' type="text" placeholder='Enter email address' />
               {/* Button */}
-              <button className=' bg-fmDarkPink hover:bg-fmCharmPink text-[15px] font-PublicSans font-[700] text-fmWaterWhite w-[173px] h-[48px] rounded-[50px] z-10'>
+              <button className=' bg-fmDarkPink hover:bg-fmCharmPink text-[15px] font-PublicSans font-[700] text-fmWaterWhite sm:w-[327px] md:w-[173px] h-[48px] rounded-[50px] z-10'>
                 Schedule a Demo
               </button>
             </div>
             {/* Question text */}
-            <div className='text-[15px] text-fmLightSanJuanBlue lg:ml-6 md:ml-[0] mt-[-20px] md:text-center lg:text-left'>
+            <div className='text-[15px] text-fmLightSanJuanBlue lg:ml-6 md:ml-[0] mt-[-20px]  sm:text-center lg:text-left sm:mb-[90px] md:mb-0'>
               <p>Have any questions? <span className='font-bold text-fmLightSanJuanBlue'>Contact Us</span></p>
             </div>
           </div>
@@ -52,7 +52,7 @@ const Home = () => {
           {/* Right Column */}
           <div className='flex flex-row md:justify-center lg:justify-end md:w-[100%] lg:w-[50%]  '>
             {/* Cellphone Image */}
-              <img className='md:h-[416px] md:w-[252px] lg:h-[660px] lg:w-[400px] mt-[-40px]  z-10' src={phone} alt="Cellphone Image" />
+              <img className='sm:h-[310px] sm:w-[195px] md:h-[416px] md:w-[252px] lg:h-[660px] lg:w-[400px] sm:mt-[-40px]  z-10' src={phone} alt="Cellphone Image" />
           </div>
         </div>
         
