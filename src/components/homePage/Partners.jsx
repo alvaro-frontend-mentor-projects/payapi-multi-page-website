0d17d1ef5ac66605b2bc749126cdbf15fb40343e
alvaroormeno <alvaro@ormeno.org>
2022-08-01 14:28:26 -0400

    partners logos complete

--- src/components/homePage/Partners.jsx
+++ src/components/homePage/Partners.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import bgCircle from '../../assets/shared/desktop/bg-pattern-circle.svg'
+
+// Import logos
+import teslaWhite from '../../assets/shared/desktop/tesla-white.svg'
+import hpWhite from '../../assets/shared/desktop/hp-white.svg'
+import googleWhite from '../../assets/shared/desktop/google-white.svg'
+import nvidiaWhite from '../../assets/shared/desktop/nvidia-white.svg'
+import oracleWhite from '../../assets/shared/desktop/oracle-white.svg'
+import microsoftWhite from '../../assets/shared/desktop/microsoft-white.svg'
 
 const Partners = () => {
-  return (
-    <div>Partners</div>
-  )
-}
+	return (
+        // MAIN CONTAINER
+		<main className="  h-[670px] bg-fmMirageBlue overflow-hidden relative mx-[-24px]">
+			{/* Background circle image */}
+			<img
+				className="absolute sm:max-w-[1000px] sm:w-[780px]  sm:top-[-602px] sm:left-[-202px]"
+				src={bgCircle}
+				alt=""
+			/>
+            {/* 1 Column Container */}
+            <div className="flex flex-col px-[24px] py-[80px]">
+                {/* Brand Section - 2 Columns */}
+                <div className="flex flex-row px-[24px] gap-10 ">
+                    {/* Left Col */}
+                    <div className="flex flex-col w-[50%] gap-10">
+                        <img src={teslaWhite} alt="tesla logo" />
+                        <img src={hpWhite} alt="hp logo" />
+                        <img src={googleWhite} alt="google logo" />
+                    </div>
+                    {/* Right Col */}
+                    <div className="flex flex-col w-[50%] gap-10">
+                        <img src={microsoftWhite} alt="microsoft logo" />
+                        <img src={oracleWhite} alt="oracle logo" />
+                        <img src={nvidiaWhite} alt="nvidia logo" />
+                    </div>
+                </div>
+                {/* Title */}
+                {/* Text */}
+                {/* About us Button */}
 
-export default Partners
+            </div>
+			
+		</main>
+	);
+};
+
+export default Partners;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,18 +3,7 @@ import bgCircle from '../assets/shared/desktop/bg-pattern-circle.svg'
 import phone from '../assets/home/desktop/illustration-phone-mockup.svg'
 import Navbar from '../components/shared/Navbar'
 import Hero from '../components/homePage/Hero'
-
-// Company Logos
-import facebook from '../assets/shared/desktop/facebook.svg'
-import google from '../assets/shared/desktop/google.svg'
-import hp from '../assets/shared/desktop/hewlett-packard.svg'
-import linkedin from '../assets/shared/desktop/linkedin.svg'
-import microsoft from '../assets/shared/desktop/microsoft.svg'
-import nvidia from '../assets/shared/desktop/nvidia.svg'
-import oracle from '../assets/shared/desktop/oracle.svg'
-import { ReactComponent as Tesla }  from '../assets/shared/desktop/tesla.svg'
-import twitter from '../assets/shared/desktop/twitter.svg'
-
+import Partners from '../components/homePage/Partners'
 
 
 
@@ -25,6 +14,7 @@ const Home = () => {
       // HOME MAIN CONTAINER
       <main className=''>
         <Hero/>
+        <Partners/>
 
         
         
@@ -33,24 +23,7 @@ const Home = () => {
 
 
         {/* [[[[[[ WHO WE WORK WITH SECTION ]]]]]] */}
-        <div className='  h-[670px] bg-fmMirageBlue overflow-hidden relative' >
-          {/* Background circle image */}
-          <img className='absolute sm:max-w-[1000px] sm:w-[780px]  sm:top-[-602px] sm:left-[-202px]' src={bgCircle} alt="" />
-          {/* Brand Section - 2 Columns */}
-          <div className='flex flex-col mt-[80px] mx-[20px]'>
-            {/* Left Col */}
-            <div className='flex flex-col w-[50%] gap-10'>
-              <Tesla fill="#FFFFFF"/>
-              <img className=" fill-white" src={hp} alt="hp logo" />
-              <img className=' fill-white' src={google} alt="google logo" />
-            </div>
-            {/* Right Col */}
-            <div className='flex flex-col w-[50%]'>
-
-            </div>
-
-          </div>
-        </div>
+        
         
        
 
