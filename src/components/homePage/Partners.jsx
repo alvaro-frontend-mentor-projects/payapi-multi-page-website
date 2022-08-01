d7e1d5a0581ef3aa06640f23c6dd8aafc1c3150f
alvaroormeno <alvaro@ormeno.org>
2022-08-01 18:24:25 -0400

    home partners component tabler view complete

--- src/components/homePage/Partners.jsx
+++ src/components/homePage/Partners.jsx
@@ -15,7 +15,7 @@ const Partners = () => {
 		<main className="  h-[670px] md:h-[615px] bg-fmMirageBlue overflow-hidden relative mx-[-24px]">
 			{/* Background circle image */}
 			<img
-				className="absolute sm:max-w-[1000px] sm:w-[780px]  sm:top-[-602px] sm:left-[-202px]"
+				className="absolute max-w-[1000px] w-[780px] sm:top-[-602px] sm:left-[-202px] md:top-[-552px] md:left-[-10px]"
 				src={bgCircle}
 				alt=""
 			/>
@@ -23,30 +23,20 @@ const Partners = () => {
             <div className="flex flex-col px-[24px] py-[80px] items-center">
                 {/* Brand Section - 2 Columns */}
                 <div className="grid grid-cols-2 md:grid-cols-3 px-[24px] gap-10 ">
-                    {/* Left Col */}
-                    
                         <img src={teslaWhite} alt="tesla logo" />
                         <img src={microsoftWhite} alt="microsoft logo" />
                         <img src={hpWhite} alt="hp logo" />
                         <img src={oracleWhite} alt="oracle logo" />
                         <img src={googleWhite} alt="google logo" />
-                        
-                        
-                    
-                    {/* Right Col */}
-                    
-                        
-                        
-                        <img src={nvidiaWhite} alt="nvidia logo" />
-                    
+                        <img src={nvidiaWhite} alt="nvidia logo" />                   
                 </div>
                 {/* Title */}
-                <div className="text-white font-SerifDisplay text-center text-[32px] mt-[40px] ">
+                <div className="text-white font-SerifDisplay text-center text-[32px] md:text-[48px] mt-[40px] md:mt-[60px] ">
                     <h1>Who we work with</h1>
                 </div>
                 {/* Text */}
                 <div className="text-center font-PublicSans text-[15px] leading-[28px] text-fmWaterWhite opacity-[70%] mt-[10px]">
-                    <p>
+                    <p className="md:w-[450px]">
                     Today, millions of people around the world have successfully connected their accounts to apps they love using our API. We provide developers with the tools they need to create easy and accessible experiences for their users. 
                     </p>
                 </div>
