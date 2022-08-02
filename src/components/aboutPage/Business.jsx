c8e4513c2965b6804b577b51519d0f17d251be94
alvaroormeno <alvaro@ormeno.org>
2022-08-02 02:24:13 -0400

    about business and vision component tablet view complete

--- src/components/aboutPage/Business.jsx
+++ src/components/aboutPage/Business.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Business = () => {
   return (
-    <div className='flex flex-col items-center gap-[16px]'>
-        <h1 className='text-[24px] text-fmSanJuanBlue font-SerifDisplay'>
+    <div className='flex flex-col md:flex-row md:justify-between items-center md:items-start gap-[16px]'>
+        <h1 className='text-[24px] md:text-[32px] text-fmSanJuanBlue font-SerifDisplay'>
             Our Business
         </h1>
-        <p className='text-[15px] text-fmLightSanJuanBlue text-center leading-[28px] '>
+        <p className='text-[15px] text-fmLightSanJuanBlue text-center md:text-left leading-[28px] md:w-[456px] '>
             At the core of our platform is the technical infrastructure APIs that connect consumers. Our innovative product provides key insights for businesses and individuals, as well as robust reporting for traditional financial institutions and developers.
         </p>
     </div>
--- src/components/aboutPage/Vision.jsx
+++ src/components/aboutPage/Vision.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Vision = () => {
   return (
-    <div className='flex flex-col items-center gap-[16px]'>
-        <h1 className='text-[24px] text-fmSanJuanBlue font-SerifDisplay'>
+    <div className='flex flex-col md:flex-row md:justify-between items-center md:items-start gap-[16px]'>
+        <h1 className='text-[24px] md:text-[32px] text-fmSanJuanBlue font-SerifDisplay'>
             Our Vision
         </h1>
-        <p className='text-[15px] text-fmLightSanJuanBlue text-center leading-[28px] '>
+        <p className='text-[15px] text-fmLightSanJuanBlue text-center md:text-left leading-[28px] md:w-[456px] '>
             Our main goal is to build beautiful consumer experiences along with developer-friendly infrastructure. The result is an intelligent tool that gives everyone the ability to create amazing products that solve big problems. We are deeply focused on democratizing financial services through technology. 
         </p>
     </div>
