ea8bf00d710bc8aa938d35038d3a5c34abd30059
alvaroormeno <alvaro@ormeno.org>
2022-08-02 02:29:17 -0400

    aboutimage component tablet view complete

--- src/components/aboutPage/AboutImage.jsx
+++ src/components/aboutPage/AboutImage.jsx
@@ -4,9 +4,11 @@ import ImageMobile from '../../assets/about/mobile/image-team-members.jpg'
 
 const AboutImage = () => {
   return (
-    <div className='flex flex-col items-center mx-[-24px] h-[300px]'>
-        <img className=' h-[300px] w-[auto]  ' src={ImageMobile} alt="" />
+    <div className='flex flex-col items-center mx-[-24px] md:mx-[-39px] h-[300px]'>
+        <img className='md:hidden h-[300px] w-[auto]  ' src={ImageMobile} alt="" />
+        <img src={ImageDesk} alt="" />
     </div>
+    
   )
 }
 
