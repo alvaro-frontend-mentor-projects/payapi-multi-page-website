0779759785a548df7ab77a1ab1758b551bff6679
alvaroormeno <alvaro@ormeno.org>
2022-08-02 02:20:12 -0400

    abouttitle component tablet view complete

--- src/components/aboutPage/AboutTitle.jsx
+++ src/components/aboutPage/AboutTitle.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const AboutTitle = () => {
   return (
-    <div className='w-[327px] mt-[48px]'>
-        <h1 className='text-[32px] font-SerifDisplay text-fmSanJuanBlue leading-[36px] text-center'>
+    <div className='w-[327px] md:w-[573px] md:mx-auto mt-[48px]'>
+        <h1 className='text-[32px] md:text-[48px] font-SerifDisplay text-fmSanJuanBlue leading-[36px] md:leading-[56px] text-center'>
         We empower innovators by delivering access to the financial system
         </h1>
     </div>
