cee6c40118c00a6e33b861626ab22794ce855c6f
alvaroormeno <alvaro@ormeno.org>
2022-08-02 02:45:05 -0400

    culture component table view complete

--- src/components/aboutPage/Culture.jsx
+++ src/components/aboutPage/Culture.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Culture = () => {
   return (
-    <div className='flex flex-col items-center gap-[16px]'>
-        <h1 className='text-[24px] text-fmSanJuanBlue font-SerifDisplay'>
+    <div className='flex flex-col md:flex-row md:justify-between items-center md:items-start gap-[16px]'>
+        <h1 className='text-[24px] md:text-[32px] text-fmSanJuanBlue font-SerifDisplay'>
             The Culture
         </h1>
-        <p className='text-[15px] text-fmLightSanJuanBlue text-center leading-[28px] '>
+        <p className='text-[15px] text-fmLightSanJuanBlue text-center md:text-left leading-[28px] md:w-[456px] '>
         We strongly believe there's always an opportunity to learn from each other outside of day-to-day work, whether it's company-wide offsites, internal hackathons, or co-worker meetups. We always value cross-team collaboration and diversity of thought, no matter the job title.
         </p>
     </div>
