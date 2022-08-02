1b1e99be6a807e0bf445987a7b2b73ef70bab32e
alvaroormeno <alvaro@ormeno.org>
2022-08-02 15:25:24 -0400

    onclick and onchange functions declared and working

--- src/components/contactPage/Form.jsx
+++ src/components/contactPage/Form.jsx
@@ -1,32 +1,56 @@
 import React from 'react'
+import { useState } from 'react'
+
+
+
+
 
 const Form = () => {
+
+const [name, setName] = useState("")
+const [email, setEmail] = useState("")
+const [company, setCompany] = useState("")
+const [title, setTitle] = useState("")
+const [message, setMessage] = useState("")
+
+console.log(name)
+
+const handleSubmit = () => {
+    console.log("submit works")
+}
+
+
   return (
     <main className=''>
-        <form className='flex flex-col md:items-center gap-[24px]' action="">
+        <form className='flex flex-col md:items-center gap-[24px]' onSubmit={handleSubmit} action="">
             <input
                 className='w-[327px] md:w-[445px] h-[42px] bg-transparent border-0 border-b-2 border-fmSanJuanBlue placeholder:text-fmSanJuanBlue placeholder:text-[15px] opacity-[50%] pl-[20px] pb-[16px] ' 
                 type="text"
-                placeholder='Name' 
+                placeholder='Name'
+                onChange={(e) => setName(e.target.value)} 
             />
             <input
                 className='w-[327px] md:w-[445px] h-[42px] bg-transparent border-0 border-b-2 border-fmSanJuanBlue placeholder:text-fmSanJuanBlue placeholder:text-[15px] opacity-[50%] pl-[20px] pb-[16px] ' 
                 type="text"
-                placeholder='Email Address' 
+                placeholder='Email Address'
+                onChange={(e) => setEmail(e.target.value)} 
             />
             <input
                 className='w-[327px] md:w-[445px] h-[42px] bg-transparent border-0 border-b-2 border-fmSanJuanBlue placeholder:text-fmSanJuanBlue placeholder:text-[15px] opacity-[50%] pl-[20px] pb-[16px] ' 
                 type="text"
-                placeholder='Company Name' 
+                placeholder='Company Name'
+                onChange={(e) => setCompany(e.target.value)} 
             />
             <input
                 className='w-[327px] md:w-[445px] h-[42px] bg-transparent border-0 border-b-2 border-fmSanJuanBlue placeholder:text-fmSanJuanBlue placeholder:text-[15px] opacity-[50%] pl-[20px] pb-[16px] ' 
                 type="text"
-                placeholder='Title' 
+                placeholder='Title'
+                onChange={(e) => setTitle(e.target.value)}
             />
             <textarea
                 className='w-[327px] md:w-[445px] h-[89px] bg-transparent border-0 border-b-2 border-fmSanJuanBlue placeholder:text-fmSanJuanBlue placeholder:text-[15px] opacity-[50%] pl-[20px] pb-[16px]'
                 placeholder='Message'
+                onChange={(e) => setMessage(e.target.value)}
             >
             </textarea>
             
