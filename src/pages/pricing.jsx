323ab3d9ca4ba76edd017549453a8c415fa7d1ab
alvaroormeno <alvaro@ormeno.org>
2022-08-01 20:28:23 -0400

    added footer to pricing page, pricing page mobile complete

--- src/pages/pricing.jsx
+++ src/pages/pricing.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import bgCircle from '../assets/shared/desktop/bg-pattern-circle.svg'
 import checks from '../assets/shared/desktop/icon-check.svg'
+import Footer from '../components/shared/Footer'
 
 const Pricing = () => {
 
@@ -136,8 +137,33 @@ const Pricing = () => {
           Request Access
         </button>
       </div>
+
+      {/* Call to action */}
+      <div className='flex flex-col items-center gap-6 mt-[80px] mb-[80px]'>
+                <h1 className='text-[32px] md:text-[48px] font-SerifDisplay text-fmSanJuanBlue'>
+                    Ready to start?
+                </h1>
+                <div className="flex flex-col md:flex-row items-center item gap-[15px] ">
+					{/* Email */}
+					<input
+						className="rounded-[50px] pl-5 pr-20 sm:mr-0 md:mr-[-35px] w-[327px] h-[48px] text-fmSanJuanBlue font-PublicSans font-[500] focus:outline-none z-10"
+						type="text"
+						placeholder="Enter email address"
+					/>
+					{/* Button */}
+					<button className=" bg-fmDarkPink hover:bg-fmCharmPink text-[15px] font-PublicSans font-[700] text-fmWaterWhite sm:w-[327px] md:w-[173px] h-[48px] md:ml-[-30px] rounded-[50px] z-10">
+						Schedule a Demo
+					</button>
+				</div>
+      </div>
+
+      {/* Footer */}
+      <Footer/>
+
       
     </div>
+
+    
 
 
   )
