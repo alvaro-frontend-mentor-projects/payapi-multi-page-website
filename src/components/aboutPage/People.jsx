37b96771c275399094fb017a5cb2d6f671e49f94
alvaroormeno <alvaro@ormeno.org>
2022-08-02 02:46:18 -0400

    people component tablet view complete

--- src/components/aboutPage/People.jsx
+++ src/components/aboutPage/People.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const People = () => {
   return (
-    <div className='flex flex-col items-center gap-[16px]'>
-        <h1 className='text-[24px] text-fmSanJuanBlue font-SerifDisplay'>
+    <div className='flex flex-col md:flex-row md:justify-between items-center md:items-start gap-[16px]'>
+        <h1 className='text-[24px] md:text-[32px] text-fmSanJuanBlue font-SerifDisplay'>
             The People
         </h1>
-        <p className='text-[15px] text-fmLightSanJuanBlue text-center leading-[28px] '>
+        <p className='text-[15px] text-fmLightSanJuanBlue text-center md:text-left leading-[28px] md:w-[456px] '>
         We're all passionate about building a more efficient and inclusive financial infrastructure together. At PayAPI, we have diverse backgrounds and skills.
         </p>
     </div>
