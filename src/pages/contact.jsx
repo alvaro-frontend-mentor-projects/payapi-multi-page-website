dbabf97a6ce0b6fd5ac295ee8fc25e17af600a74
alvaroormeno <alvaro@ormeno.org>
2022-08-02 13:39:37 -0400

    added calltoaction and footer components to contact page mobile view

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 import ContactTitle from '../components/contactPage/ContactTitle'
 import Form from '../components/contactPage/Form'
 import ContactPartners from '../components/contactPage/ContactPartners'
+import CallAction from '../components/shared/CallAction'
+import Footer from '../components/shared/Footer'
 
 const Contact = () => {
   return (
     <main>
         <ContactTitle/>
-        <Form/>
-        <ContactPartners/>
+        <div className='flex flex-col gap-[80px]'>
+          <Form/>
+          <ContactPartners/>
+          <CallAction/>
+          <Footer/>
+        </div>
+        
 
     </main>
   )
