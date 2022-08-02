7140ce2f25d02fd5a59a63311ca4853afad7d832
alvaroormeno <alvaro@ormeno.org>
2022-08-02 13:55:49 -0400

    contactpartners component tablet view complete

--- src/components/contactPage/ContactPartners.jsx
+++ src/components/contactPage/ContactPartners.jsx
@@ -10,10 +10,10 @@ import microsoft from '../../assets/shared/desktop/microsoft.svg'
 const ContactPartners = () => {
   return (
     <div className='flex flex-col items-center gap-[32px]'>
-        <h1 className='text-[24px] md:text-[48px] lg:text-[56px] font-SerifDisplay text-fmSanJuanBlue opacity-[75%] leading-[32px] md:leading-[56px] text-center lg:text-left'>
+        <h1 className='text-[24px] md:text-[24px] lg:text-[56px] font-SerifDisplay text-fmSanJuanBlue opacity-[75%] leading-[32px] md:leading-[32px] text-center lg:text-left md:w-[445px]'>
             Join the thousands of innovators already building with us
         </h1>
-        <div className="grid grid-cols-2 w-[280px] md:grid-cols-3 gap-[42px] ">
+        <div className="grid grid-cols-2 w-[280px] md:w-[541px] md:grid-cols-3 gap-[42px] ">
                         <img src={tesla} alt="tesla logo" />
                         <img src={microsoft} alt="microsoft logo" />
                         <img src={hp} alt="hp logo" />
