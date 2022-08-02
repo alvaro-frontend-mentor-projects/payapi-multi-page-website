fccce51685644a3692334512e64df2d5140b60d9
alvaroormeno <alvaro@ormeno.org>
2022-08-02 03:04:53 -0400

    numbers component for aboutpage desktop view complete

--- src/components/aboutPage/Numbers.jsx
+++ src/components/aboutPage/Numbers.jsx
@@ -2,38 +2,38 @@ import React from 'react'
 
 const Numbers = () => {
   return (
-    <main className='flex flex-col md:flex-row mt-[48px] md:gap-2'>
+    <main className='flex flex-col md:flex-row mt-[48px] lg:mt-[72px] md:gap-2 lg:gap-8'>
         <div className='flex flex-col md:items-start'>
-            <hr className='w-[327px] md:w-[223px] border border-fmLightSanJuanBlue opacity-[25%] mb-4 md:mb-0' />
+            <hr className='w-[327px] md:w-[223px] lg:w-[350px] border border-fmLightSanJuanBlue opacity-[25%] mb-4 md:mb-0' />
             <p className='text-[16px] font-PublicSans text-fmSanJuanBlue opacity-[70%] text-center md:mt-6'>
                 Team Members
             </p>
             <h1 className='text-[56px] text-center text-fmDarkPink font-SerifDisplay'>
                 300+
             </h1>
-            <hr className='w-[327px] md:w-[223px] border border-fmLightSanJuanBlue opacity-[25%] hidden md:block' />
+            <hr className='w-[327px] md:w-[223px] lg:w-[350px] border border-fmLightSanJuanBlue opacity-[25%] hidden md:block' />
         </div>
 
         <div className='flex flex-col md:items-start'>
-            <hr className='w-[327px] md:w-[223px] border border-fmLightSanJuanBlue opacity-[25%] hidden md:block' />
+            <hr className='w-[327px] md:w-[223px] lg:w-[350px] border border-fmLightSanJuanBlue opacity-[25%] hidden md:block' />
             <p className='text-[16px] font-PublicSans text-fmSanJuanBlue opacity-[70%] text-center md:mt-6'>
                 Offices in the US
             </p>
             <h1 className='text-[56px] text-center text-fmDarkPink font-SerifDisplay'>
                 3
             </h1>
-            <hr className='w-[327px] md:w-[223px] border border-fmLightSanJuanBlue opacity-[25%] hidden md:block' />
+            <hr className='w-[327px] md:w-[223px] lg:w-[350px] border border-fmLightSanJuanBlue opacity-[25%] hidden md:block' />
         </div>
 
         <div className='flex flex-col md:items-start'>
-            <hr className='w-[327px] md:w-[223px] border border-fmLightSanJuanBlue opacity-[25%] hidden md:block' />
+            <hr className='w-[327px] md:w-[223px] lg:w-[350px] border border-fmLightSanJuanBlue opacity-[25%] hidden md:block' />
             <p className='text-[16px] font-PublicSans text-fmSanJuanBlue opacity-[70%] text-center md:mt-6'>
                 Transactions analyzed
             </p>
             <h1 className='text-[56px] text-center text-fmDarkPink font-SerifDisplay'>
                 10M+
             </h1>
-            <hr className='w-[327px] md:w-[223px] border border-fmLightSanJuanBlue opacity-[25%] mt-4 md:mt-0' />
+            <hr className='w-[327px] md:w-[223px] lg:w-[350px] border border-fmLightSanJuanBlue opacity-[25%] mt-4 md:mt-0' />
         </div>
         
 
