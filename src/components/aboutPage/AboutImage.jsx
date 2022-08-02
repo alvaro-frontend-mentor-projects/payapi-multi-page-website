84f2d665f411c0249fe2642de0801ef6e700b32b
alvaroormeno <alvaro@ormeno.org>
2022-08-02 03:01:49 -0400

    aboutimage desktop view complete

--- src/components/aboutPage/AboutImage.jsx
+++ src/components/aboutPage/AboutImage.jsx
@@ -4,7 +4,7 @@ import ImageMobile from '../../assets/about/mobile/image-team-members.jpg'
 
 const AboutImage = () => {
   return (
-    <div className='flex flex-col items-center mx-[-24px] md:mx-[-39px] h-[300px]'>
+    <div className='flex flex-col items-center mx-[-24px] md:mx-[-39px] lg:mx-[-165px] h-[300px] lg:h-[100%]'>
         <img className='md:hidden h-[300px] w-[auto]  ' src={ImageMobile} alt="" />
         <img className='hidden md:block' src={ImageDesk} alt="" />
     </div>
