8ef4d71a5be22a81c3e2c196eb136d1b10ef83cf
alvaroormeno <alvaro@ormeno.org>
2022-08-02 13:42:04 -0400

    contact page, added bgcircle, page mobile view complete

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -4,11 +4,13 @@ import Form from '../components/contactPage/Form'
 import ContactPartners from '../components/contactPage/ContactPartners'
 import CallAction from '../components/shared/CallAction'
 import Footer from '../components/shared/Footer'
+import bgCircle from '../assets/shared/desktop/bg-pattern-circle.svg'
 
 const Contact = () => {
   return (
     <main>
         <ContactTitle/>
+        <img className=' absolute max-w-[1500px] w-[780px] md:w-[1000px] right-[-470px] top-[-650px] md:right-[-700px] md:top-[-700px] lg:w-[780px] lg:left-[800px] lg:top-[-527px] z-0' src={bgCircle} alt="" />
         <div className='flex flex-col gap-[80px]'>
           <Form/>
           <ContactPartners/>
