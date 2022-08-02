7b1aa96c5c6372e4ebcf487c91f78edf94cfccb5
alvaroormeno <alvaro@ormeno.org>
2022-08-01 21:25:50 -0400

    fixed home desketip view, complete

--- src/components/homePage/Partners.jsx
+++ src/components/homePage/Partners.jsx
@@ -12,15 +12,15 @@ import microsoftWhite from '../../assets/shared/desktop/microsoft-white.svg'
 const Partners = () => {
 	return (
         // MAIN CONTAINER
-		<main className="  h-[670px] md:h-[615px] bg-fmMirageBlue overflow-hidden relative mx-[-39px] lg:mx-[-165px]">
+		<main className="  h-[670px] md:h-[615px] lg:h-[472px] bg-fmMirageBlue overflow-hidden relative mx-[-39px] lg:mx-[-165px] px-[24px] lg:px-[165px] ">
 			{/* Background circle image */}
 			<img
-				className="absolute max-w-[1000px] w-[780px] sm:top-[-602px] sm:left-[-202px] md:top-[-552px] md:left-[-10px]"
+				className="absolute max-w-[1000px] w-[780px] sm:top-[-602px] sm:left-[-202px] md:top-[-552px] md:left-[-10px] lg:top-[-359px] lg:left-[-235px]"
 				src={bgCircle}
 				alt=""
 			/>
             {/* 1 Column Container */}
-            <div className="flex flex-col px-[24px] py-[80px] items-center">
+            <div className="flex flex-col lg:flex-row-reverse lg:justify-between  px-[24px] lg:px-[0px]  py-[80px] items-center">
                 {/* Brand Section - 2 Columns */}
                 <div className="grid grid-cols-2 md:grid-cols-3 px-[24px] gap-10 ">
                         <img src={teslaWhite} alt="tesla logo" />
@@ -30,20 +30,24 @@ const Partners = () => {
                         <img src={googleWhite} alt="google logo" />
                         <img src={nvidiaWhite} alt="nvidia logo" />                   
                 </div>
-                {/* Title */}
-                <div className="text-white font-SerifDisplay text-center text-[32px] md:text-[48px] mt-[40px] md:mt-[60px] ">
-                    <h1>Who we work with</h1>
+                {/* Info Container */}
+                <div className="flex flex-col items-center lg:items-start lg:justify-center ">
+                    {/* Title */}
+                    <div className="text-white font-SerifDisplay text-center text-[32px] md:text-[48px] mt-[40px] md:mt-[60px] lg:mt-[0px] ">
+                        <h1>Who we work with</h1>
+                    </div>
+                    {/* Text */}
+                    <div className="text-center lg:text-left font-PublicSans text-[15px] leading-[28px] text-fmWaterWhite opacity-[70%] mt-[10px]">
+                        <p className="md:w-[450px]">
+                        Today, millions of people around the world have successfully connected their accounts to apps they love using our API. We provide developers with the tools they need to create easy and accessible experiences for their users. 
+                        </p>
+                    </div>
+                    {/* About us Button */}
+                    <button className="border rounded-[50px] h-[48px] w-[129px] text-fmWaterWhite hover:text-fmMirageBlue hover:bg-fmWaterWhite mt-[40px]">
+                        About Us
+                    </button>
                 </div>
-                {/* Text */}
-                <div className="text-center font-PublicSans text-[15px] leading-[28px] text-fmWaterWhite opacity-[70%] mt-[10px]">
-                    <p className="md:w-[450px]">
-                    Today, millions of people around the world have successfully connected their accounts to apps they love using our API. We provide developers with the tools they need to create easy and accessible experiences for their users. 
-                    </p>
-                </div>
-                {/* About us Button */}
-                <button className="border rounded-[50px] h-[48px] w-[129px] text-fmWaterWhite hover:text-fmMirageBlue hover:bg-fmWaterWhite mt-[40px]">
-                    About Us
-                </button>
+                
 
             </div>
 			
